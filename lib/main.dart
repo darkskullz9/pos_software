101028329791f7656c7b93b3a5ce4492a4c8f121
Darkskullz9 <darxkullz9@gmail.com>
2026-06-17 16:48:48 +0200

    Refactor main.dart for improved readability; adjust import order and format runApp parameters

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
-import 'app/app.dart';
-import 'data/services/product_service.dart';
-import 'data/services/barcode_service.dart';
 
-void main() {
+import 'app/app.dart';
+import 'data/services/barcode_service.dart';
+import 'data/services/product_service.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   final productService = ProductService();
   final barcodeService = BarcodeService();
 
   runApp(
-    CaisseApp(productService: productService, barcodeService: barcodeService),
+    CaisseApp(
+      productService: productService,
+      barcodeService: barcodeService,
+    ),
   );
 }
