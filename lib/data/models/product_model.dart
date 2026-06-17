644675a21df32e9fd4a4fc6a78ea099149a3a29d
Darkskullz9 <darxkullz9@gmail.com>
2026-06-17 16:36:29 +0200

    Add optional id field to ProductModel and update toMap/fromMap methods

--- lib/data/models/product_model.dart
+++ lib/data/models/product_model.dart
@@ -1,4 +1,5 @@
 class ProductModel {
+  final int? id;
   final String name;
   final String? brand;
   final double price;
@@ -9,6 +10,7 @@ class ProductModel {
   final int? sizeCode;
 
   const ProductModel({
+    this.id,
     required this.name,
     this.brand,
     required this.price,
@@ -20,6 +22,7 @@ class ProductModel {
   });
 
   ProductModel copyWith({
+    int? id,
     String? name,
     String? brand,
     double? price,
@@ -30,6 +33,7 @@ class ProductModel {
     int? sizeCode,
   }) {
     return ProductModel(
+      id: id ?? this.id,
       name: name ?? this.name,
       brand: brand ?? this.brand,
       price: price ?? this.price,
@@ -38,6 +42,34 @@ class ProductModel {
       categoryCode: categoryCode ?? this.categoryCode,
       colorCode: colorCode ?? this.colorCode,
       sizeCode: sizeCode ?? this.sizeCode,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'brand': brand,
+      'price': price,
+      'stock': stock,
+      'barcode': barcode,
+      'category_code': categoryCode,
+      'color_code': colorCode,
+      'size_code': sizeCode,
+    };
+  }
+
+  factory ProductModel.fromMap(Map<String, dynamic> map) {
+    return ProductModel(
+      id: map['id'] as int?,
+      name: map['name'] as String,
+      brand: map['brand'] as String?,
+      price: (map['price'] as num).toDouble(),
+      stock: map['stock'] as int,
+      barcode: map['barcode'] as String?,
+      categoryCode: map['category_code'] as int,
+      colorCode: map['color_code'] as int?,
+      sizeCode: map['size_code'] as int?,
     );
   }
 }
