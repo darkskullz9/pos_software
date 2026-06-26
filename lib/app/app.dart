df43ecb313a6cea6242d7bc388de3ac7b58f41f6
Darkskullz9 <darxkullz9@gmail.com>
2026-06-26 13:46:44 +0200

    Wire settings service into app:
    Passes settings preferences to AppShell.

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -3,15 +3,18 @@ import 'package:flutter/material.dart';
 import '../core/widgets/app_shell.dart';
 import '../data/services/barcode_service.dart';
 import '../data/services/product_service.dart';
+import '../data/services/settings_service.dart';
 
 class CaisseApp extends StatelessWidget {
   final ProductService productService;
   final BarcodeService barcodeService;
+  final SettingsService settingsService;
 
   const CaisseApp({
     super.key,
     required this.productService,
     required this.barcodeService,
+    required this.settingsService,
   });
 
   @override
@@ -26,6 +29,7 @@ class CaisseApp extends StatelessWidget {
       home: AppShell(
         productService: productService,
         barcodeService: barcodeService,
+        settingsService: settingsService,
       ),
     );
   }
