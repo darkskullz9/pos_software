0f4fe9cbb6940ef924721144b90e670259bd9421
darkskullz9 <darxkullz9@gmail.com>
2026-07-02 13:05:42 +0200

    Add libsqlite3.so and pos_software binary; include copyright file
    - Added a new shared library `libsqlite3.so` for database management.
    - Introduced the `pos_software` binary for point of sale functionalities.
    - Created a copyright file detailing ownership and usage rights for the software.

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -49,7 +49,7 @@ static void my_application_activate(GApplication* application) {
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "flutter_application_1");
+    gtk_window_set_title(window, "POS Software");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
